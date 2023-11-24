3bcc147b19e7c06bbb358b247753885f8131bb09
aditya <adityakeche567@gmail.com>
2023-11-24 20:41:22 +0700

    pesan commit

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\API\ProductController;
-use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\ProductCategoryController;
 
 /*
 |--------------------------------------------------------------------------
